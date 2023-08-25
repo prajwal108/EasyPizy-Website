abea1773956a9e62d6b6f50af932b5b91cb80b0b
prajwal108 <prajwalk8976@gmail.com>
2023-08-25 20:11:27 +0530

    loin form css

--- js/slide.js
+++ js/slide.js
@@ -1,26 +1,4 @@
 
-// window.addEventListener("scroll", function() {
-//   const heroSectionHeight = document.querySelector(".hero-section").offsetHeight;
-//   const navbar = document.querySelector(".navbar");
-
-//   if (window.scrollY > heroSectionHeight) {
-//     navbar.classList.add("transparent");
-//   } else {
-//     navbar.classList.remove("transparent");
-//   }
-// });
-
-// window.addEventListener("scroll", function () {
-//   const SectionHeight = document.querySelector(".site-header").offsetHeight;
-//   const navbar = document.querySelector(".navbar");
-
-//   if (window.scrollY > SectionHeight) {
-//     navbar.classList.add("transparent");
-//   } else {
-//     navbar.classList.remove("transparent");
-//   }
-// });
-
 function sendToWhatsApp() {
   // Get form data
   const fullName = encodeURIComponent(
@@ -54,8 +32,8 @@ const accountText = document.getElementById("accountText");
 const accountMenu = document.getElementById("accountMenu");
 
 // Check if the user is logged in or signed up
-const isLoggedIn = true; // Set this to true if the user is logged in
-const isSignedUp = true; // Set this to true if the user is signed up
+const isLoggedIn = false; // Set this to true if the user is logged in
+const isSignedUp = false; // Set this to true if the user is signed up
 
 if (isLoggedIn || isSignedUp) {
     // Update the account text and display additional options
@@ -73,3 +51,63 @@ if (isLoggedIn || isSignedUp) {
 
 
 
+
+// Add this script before the closing </body> tag
+const modalOverlay = document.getElementById("modalOverlay");
+const modal = document.getElementById("modal");
+const closeModalBtn = document.getElementById("closeModal");
+const LoginBtn = document.getElementById("loginBtn");
+const LoginBtnS = document.getElementById("loginBtnS");
+
+// Show the modal when "Login/Signup" is clicked
+LoginBtn.addEventListener("click", () => {
+  modalOverlay.style.display = "flex";
+  modal.style.display = "block";
+
+});
+// Show the modal when "Login/Signup" is clicked
+LoginBtnS.addEventListener("click", () => {
+  modalOverlay.style.display = "flex";
+  modal.style.display = "block";
+});
+
+// Close the modal when the close button is clicked
+closeModalBtn.addEventListener("click", () => {
+  modalOverlay.style.display = "none";
+});
+
+// Close the modal if user clicks outside the modal content
+window.addEventListener("click", (event) => {
+  if (event.target === modalOverlay) {
+    modalOverlay.style.display = "none";
+  }
+});
+
+
+const loginForm = document.getElementById("loginForm");
+const sendOtpBtn = document.getElementById("sendOtpBtn");
+
+// Function to handle sending OTP
+sendOtpBtn.addEventListener("click", () => {
+  const phoneNumber = document.getElementById("phoneNumber").value;
+
+  firebase
+    .auth()
+    .signInWithPhoneNumber(phoneNumber, recaptchaVerifier)
+    .then((confirmationResult) => {
+      const verificationCode = prompt("Enter OTP sent to your phone:");
+      return confirmationResult.confirm(verificationCode);
+    })
+    .then((userCredential) => {
+      // User is logged in or signed up
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+// Function to handle form submission
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Your form submission logic
+});
