caf23fd71564e86ca80107ece11affaeffdbf5e6
prajwal108 <prajwalk8976@gmail.com>
2023-08-27 20:38:59 +0530

    product pae js

--- js/products.js
+++ js/products.js
@@ -1,40 +1,43 @@
 // Get necessary elements
-const cashIcon = document.getElementById("cash");
-const finalPriceElement1 = document.querySelector(".final-price-1");
-const originalPriceElement1 = document.querySelector(".original-price-1");
-const discountElement1 = document.querySelector(".discount-1");
-const sizeRadiosPulav = document.querySelectorAll("input[type='radio']");
 const addToCartButton = document.getElementById("addToCart");
 const buyNowButton1 = document.getElementById("buyNowBtn1");
 
-// Function to calculate and update prices based on selected size
-function updatePricesPulav(selectedValue) {
-  let price = 100; // Default price
-  let discount = 20; // Default discount percentage
+window.addEventListener("load", () => {
+  const sizeRadios = document.querySelectorAll("input[type='radio']");
+  const finalPrice = document.querySelector(".final-price-1");
+  const originalPrice = document.querySelector(".original-price-1");
+  const discountText = document.querySelector(".discount-1");
 
-  if (selectedValue === "500gm") {
-    price = 200;
-    discount = 25;
-  } else if (selectedValue === "1000gm") {
-    price = 400;
-    discount = 30;
-  }
+  // Load selected size from local storage or use default value
+  const selectedSize = "250gm";
 
-  const finalPriceValue = price - (price * discount) / 100;
-  finalPriceElement1.textContent = `₹${finalPriceValue}`;
-  originalPriceElement1.textContent = `₹${price}`;
-  discountElement1.textContent = `${discount}% off`;
-}
+  // Set radio buttons and prices based on the selected size
+  sizeRadios.forEach((radio) => {
+    radio.checked = radio.value === selectedSize;
+    radio.addEventListener("change", function () {
+      // Update local storage with the selected size
+      localStorage.setItem("selectedSize_pulav", this.value);
 
-// Attach event listeners to size radios
-sizeRadiosPulav.forEach((radio) => {
-  radio.addEventListener("change", function () {
-    updatePricesPulav(this.value);
+      // Simulated dynamic price and discount calculation
+      const selectedValue = this.value;
+      let price = 100; // Default price
+      let discount = 20; // Default discount percentage
+
+      if (selectedValue === "500gm") {
+        price = 200;
+        discount = 25;
+      } else if (selectedValue === "1000gm") {
+        price = 400;
+        discount = 30;
+      }
+
+      const finalPriceValue = price - (price * discount) / 100;
+      finalPrice.textContent = `₹${finalPriceValue}`;
+      originalPrice.textContent = `₹${price}`;
+      discountText.textContent = `${discount}% off`;
+    });
   });
 });
-
-// Initial update of prices based on default selected size
-updatePricesPulav("250gm");
 
 // Add to Cart button click event
 addToCartButton.addEventListener("click", function (event) {
