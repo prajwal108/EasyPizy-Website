2c5f5a98dde149b701145edd014bb5ca022a77c6
prajwal108 <prajwalk8976@gmail.com>
2023-08-28 15:32:59 +0530

    recommendation corousel done

--- js/products.js
+++ js/products.js
@@ -77,3 +77,25 @@ minusBtn.addEventListener("click", function (event){
   }
 });
 
+//  related owl container
+// Related carousel
+    $('.related-carousel').owlCarousel({
+        loop: true,
+        margin: 29,
+        nav: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+    
