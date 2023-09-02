6538bb49d4841ed50cbfb387d2196dacacacb71e
prajwal108 <prajwalk8976@gmail.com>
2023-09-02 22:36:09 +0530

    firebase aut added

--- js/firebase.js
+++ js/firebase.js
@@ -1,91 +1,99 @@
   // TODO: Add SDKs for Firebase products that you want to use
+//  import firebase from "firebase/app";
+ const firebase = require("firebase/app");
+  require("firebase/auth");
+  require("firebase/firestore");
+  require("firebase/analytics");
+  require("firebase/database");
+
+
   // https://firebase.google.com/docs/web/setup#available-libraries
+     const firebaseConfig = {
+       apiKey: "AIzaSyA4bdQPLI8i7SgvjUj9eKLirPuUWvYUXcI",
+       authDomain: "easypizy-in.firebaseapp.com",
+       databaseURL:
+         "https://easypizy-in-default-rtdb.asia-southeast1.firebasedatabase.app",
+       projectId: "easypizy-in",
+       storageBucket: "easypizy-in.appspot.com",
+       messagingSenderId: "860069269388",
+       appId: "1:860069269388:web:b44033d7d721143eebd0a9",
+       measurementId: "G-SYY5K0GJ7H",
+     };
 
   // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyA4bdQPLI8i7SgvjUj9eKLirPuUWvYUXcI",
-    authDomain: "easypizy-in.firebaseapp.com",
-    databaseURL: "https://easypizy-in-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "easypizy-in",
-    storageBucket: "easypizy-in.appspot.com",
-    messagingSenderId: "860069269388",
-    appId: "1:860069269388:web:b44033d7d721143eebd0a9",
-    measurementId: "G-SYY5K0GJ7H"
-  };
+// Your web app's Firebase configuration
+   let confirmationResult;
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ document.addEventListener("DOMContentLoaded", () => {
+   // Your Firebase and event listener code here
+   // Initialize Firebase
+   const app = firebase.initializeApp(firebaseConfig);
+   const analytics = getAnalytics(app);
+   const auth = getAuth(app);
 
-  document.addEventListener("DOMContentLoaded", () => {
-    // Your Firebase and event listener code here
+   const sendOtpButton = document.getElementById("sendOtpBtn");
+   const phoneNumberInput = document.getElementById("phoneNumber");
+   let isButtonDisabled = false; // Track whether the button is disabled
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-    const auth = getAuth(app);
+   sendOtpButton.addEventListener("click", () => {
+     if (isButtonDisabled) {
+       return; // Don't do anything if the button is already disabled
+     }
 
-    const sendOtpButton = document.getElementById("sendOtpBtn");
-    const phoneNumberInput = document.getElementById("phoneNumber");
-    let confirmationResult;
-    let isButtonDisabled = false; // Track whether the button is disabled
+     const phoneNumber = "+91" + phoneNumberInput.value; // Construct the full phone number
 
-    sendOtpButton.addEventListener("click", () => {
-      if (isButtonDisabled) {
-        return; // Don't do anything if the button is already disabled
-      }
+     // Create reCAPTCHA verifier
+     const appVerifier = new firebase.auth.RecaptchaVerifier(
+       "recaptcha-container",
+       {
+         size: "invisible", // Set to 'invisible' for invisible reCAPTCHA
+       }
+     );
 
-      const phoneNumber = "+91" + phoneNumberInput.value; // Construct the full phone number
+     // Disable the button
+     sendOtpButton.disabled = true;
+     isButtonDisabled = true;
 
-      // Create reCAPTCHA verifier
-      const appVerifier = new firebase.auth.RecaptchaVerifier(
-        "recaptcha-container",
-        {
-          size: "invisible", // Set to 'invisible' for invisible reCAPTCHA
-        }
-      );
+     // Send OTP to the user's phone
+     signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+       .then((confirmationResult) => {
+         // OTP sent successfully, proceed to verification
+         // Save the 'confirmationResult' to use in the next step
 
-      // Disable the button
-      sendOtpButton.disabled = true;
-      isButtonDisabled = true;
+         // Set a timer to re-enable the button after 1 minute (60,000 milliseconds)
+         setTimeout(() => {
+           sendOtpButton.disabled = false;
+           isButtonDisabled = false;
+         }, 60000); // 1 minute
+       })
+       .catch((error) => {
+         console.error("Error sending OTP:", error);
+         // Handle error
 
-      // Send OTP to the user's phone
-      signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-        .then((confirmationResult) => {
-          // OTP sent successfully, proceed to verification
-          // Save the 'confirmationResult' to use in the next step
+         // Re-enable the button in case of an error
+         sendOtpButton.disabled = false;
+         isButtonDisabled = false;
+       });
+   });
 
-          // Set a timer to re-enable the button after 1 minute (60,000 milliseconds)
-          setTimeout(() => {
-            sendOtpButton.disabled = false;
-            isButtonDisabled = false;
-          }, 60000); // 1 minute
-        })
-        .catch((error) => {
-          console.error("Error sending OTP:", error);
-          // Handle error
+   const otpInput = document.getElementById("otp");
+   const verifyAndLoginButton = document.getElementById("verifyAndLoginBtn");
 
-          // Re-enable the button in case of an error
-          sendOtpButton.disabled = false;
-          isButtonDisabled = false;
-        });
-    });
+   verifyAndLoginButton.addEventListener("click", () => {
+     const otp = otpInput.value;
 
-    const otpInput = document.getElementById("otp");
-    const verifyAndLoginButton = document.getElementById("verifyAndLoginBtn");
-
-    verifyAndLoginButton.addEventListener("click", () => {
-      const otp = otpInput.value;
-
-      // Use the confirmationResult from the previous step to verify the OTP
-      confirmationResult
-        .confirm(otp)
-        .then((userCredential) => {
-          // User successfully authenticated, you can now access userCredential.user
-          const user = userCredential.user;
-          console.log("User signed in:", user);
-          // Redirect or perform other actions as needed
-        })
-        .catch((error) => {
-          console.error("Error verifying OTP:", error);
-          // Handle error
-        });
-    });
-  });
+     // Use the confirmationResult from the previous step to verify the OTP
+     confirmationResult
+       .confirm(otp)
+       .then((userCredential) => {
+         // User successfully authenticated, you can now access userCredential.user
+         const user = userCredential.user;
+         console.log("User signed in:", user);
+         // Redirect or perform other actions as needed
+       })
+       .catch((error) => {
+         console.error("Error verifying OTP:", error);
+         // Handle error
+       });
+   });
+ });
