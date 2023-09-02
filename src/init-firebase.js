86ede885b66c2466f78ac8a946aaa74355798091
prajwal108 <prajwalk8976@gmail.com>
2023-09-02 19:35:50 +0530

    firebase aut

---
+++ src/init-firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA4bdQPLI8i7SgvjUj9eKLirPuUWvYUXcI",
+  authDomain: "easypizy-in.firebaseapp.com",
+  databaseURL:
+    "https://easypizy-in-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "easypizy-in",
+  storageBucket: "easypizy-in.appspot.com",
+  messagingSenderId: "860069269388",
+  appId: "1:860069269388:web:b44033d7d721143eebd0a9",
+  measurementId: "G-SYY5K0GJ7H",
+};
+
+firebase.initializeApp(firebaseConfig);
