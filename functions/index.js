15a2f0ec234f8f0ba5f9ebbaa8b0ba3ab141ecdc
prajwal108 <prajwalk8976@gmail.com>
2023-10-05 16:18:01 +0530

    clod function test

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 const cors = require("cors")({origin: true});
 const Razorpay = require("razorpay");
 
@@ -27,3 +29,119 @@ exports.createOrder = functions
         });
       });
     });
+
+
+exports.processOrder = functions
+    .region("asia-south1")
+    .https.onRequest((request, response) => {
+      cors(request, response, async () => {
+        try {
+          const {
+            amount,
+            orderID,
+            name,
+            contact,
+            email, // Default to an empty string if not provided
+            userUID,
+          } = request.body;
+
+
+          const options = {
+            key: {
+              key_id: functions.config().razorpay.key_id,
+              key_secret: functions.config().razorpay.key_secret,
+            },
+            amount: amount,
+            currency: "INR",
+            name: "EasyPizy", // your business name
+            order_id: orderID,
+            callback_url: "127.0.0.1:5501/Account-Menu/order-history.html",
+            prefill: {
+              name: name, // your customer's name
+              email: email,
+              contact: contact,
+            },
+            theme: {
+              color: "#ff6347",
+            },
+            modal: {
+              confirm_close: true,
+            },
+            customer_id: userUID,
+            remember_customer: true,
+            hidden: {
+              contact: true,
+              email: true,
+            },
+            send_sms_hash: true,
+          };
+
+          const rzp1 = new Razorpay(options);
+
+
+          rzp1.open();
+
+          rzp1.on("payment.success", (response) => {
+            // Payment success logic here
+            console.log("Payment successful:", response);
+
+            // Store the payment details in Firestore
+            const paymentDetails = {
+              razorpay_payment_id: response.razorpay_payment_id,
+              razorpay_order_id: response.razorpay_order_id,
+              razorpay_signature: response.razorpay_signature,
+            };
+
+            // Define the path to the Firestore document
+            const orderDocumentPath = `users/${userUID}/orders/${orderID}`;
+
+            // Get a Firestore reference to the document
+            const orderDocRef = admin.firestore().doc(orderDocumentPath);
+
+            // Set the payment details in Firestore
+            orderDocRef
+                .update(paymentDetails, {merge: true})
+                .then(() => {
+                  console.log("Payment details stored in Firestore.");
+                })
+                .catch((error) => {
+                  console.error(
+                      "Error storing payment details in Firestore:",
+                      error,
+                  );
+                });
+
+            // Send a success response to the client
+            response.status(200).send("Payment successful");
+          });
+
+          rzp1.on("payment.error", async (error) => {
+            // Payment error logic here
+            console.error("Payment error:", error);
+
+            // Send an error response to the client
+            response.status(500).send("Payment error");
+
+            // Update the status field in Firestore to "failed"
+            const orderDocumentPath = `users/${userUID}/orders/${orderID}`;
+            const orderDocRef = admin.firestore().doc(orderDocumentPath);
+
+            try {
+              await orderDocRef.update({status: "failed"}, {merge: true});
+              console.log("Order status updated to 'failed' in Firestore.");
+            } catch (updateError) {
+              console.error(
+                  "Error updating order status in Firestore:",
+                  updateError,
+              );
+            }
+          });
+        } catch (error) {
+          console.error("Error processing the order:", error);
+          // Send an error response to the client in case of any exception
+          response.status(500).send("Error processing the order");
+        }
+      });
+    });
+
+
